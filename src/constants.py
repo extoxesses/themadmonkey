1afd10c50df4c0527f40e0b69ca3dfbf88624775
exto <extoxesses@gmail.com>
2019-06-11 23:10:25 +0200

    Updated project to Python3

--- src/constants.py
+++ src/constants.py
@@ -8,6 +8,7 @@ LOGGER_FORMAT = '[%(asctime)s] %(levelname)s\t%(name)s\t%(message)s'
 MAIN = 'main'
 CUSTOM_PACKAGE = 'custom'
 HELP_PACKAGE = 'helper'
+MSG_PACKAGE = 'messages'
 NETWORK_PACKAGE = 'network'
 USERS_PACKAGE = 'users'
 
